9b827a3f4c155fe0021654ad56f7f56f0857d456
Vovodroid <vovodroid@users.noreply.github.com>
2023-05-14 19:41:36 +0300

    Save also STL in recent file list, increase list size to 15.

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -4126,8 +4126,10 @@ void Plater::add_model(bool imperial_units/* = false*/)
         return;
 
     std::vector<fs::path> paths;
-    for (const auto &file : input_files)
+    for (const auto &file : input_files) {
         paths.emplace_back(into_path(file));
+        wxGetApp().mainframe->add_to_recent_projects(file);
+    }
 
     wxString snapshot_label;
     assert(! paths.empty());
